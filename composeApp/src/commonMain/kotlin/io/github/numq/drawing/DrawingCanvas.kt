68688dd90bbee9ee40998cd22b35e9ce87435f2e
numq <numq.dev@gmail.com>
2025-07-05 02:52:11 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/drawing/DrawingCanvas.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/drawing/DrawingCanvas.kt
@@ -3,144 +3,107 @@ package io.github.numq.drawing
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onSizeChanged
-import io.github.numq.recognition.TextRecognitionService
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun DrawingCanvas(initialMode: DrawingMode = DrawingMode.DRAW, recognitionService: TextRecognitionService) {
-    val coroutineScope = rememberCoroutineScope()
-
-    var recognizedText by remember { mutableStateOf("Nothing recognized") }
+fun DrawingCanvas(
+    modifier: Modifier,
+    initialMode: DrawingMode = DrawingMode.DRAW,
+    strokeWidth: Float = 8f,
+    onContentChange: (IntArray) -> Unit
+) {
+    val canvasDrawScope = remember { CanvasDrawScope() }
 
     var mode by remember { mutableStateOf(initialMode) }
 
     val paths = remember { mutableStateListOf<Path>() }
 
-    var currentStroke by remember { mutableStateOf<List<Offset>>(emptyList()) }
+    val points = remember { mutableStateListOf<Offset>() }
 
-    val strokeColor = if (mode == DrawingMode.DRAW) Color.Black else Color.White
+    BoxWithConstraints(modifier = modifier) {
+        val canvasSize = remember(maxWidth, maxHeight) {
+            Size(maxWidth.value, maxHeight.value)
+        }
 
-    val strokeWidth = if (mode == DrawingMode.DRAW) 8f else 40f
+        val canvasBitmap = remember(canvasSize) {
+            ImageBitmap(canvasSize.width.toInt(), canvasSize.height.toInt())
+        }
 
-    var canvasSize by remember { mutableStateOf<Size>(Size.Zero) }
+        Box(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
+            detectDragGestures(onDragStart = { offset ->
+                points.add(offset)
+            }, onDrag = { change, _ ->
+                points.add(change.position)
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        BoxWithConstraints(Modifier.fillMaxWidth().fillMaxHeight(.9f).onSizeChanged { size ->
-            canvasSize = Size(width = size.width.toFloat(), height = size.height.toFloat())
-        }.pointerInput(mode, strokeColor, strokeWidth) {
-            detectDragGestures(
-                onDragStart = { offset ->
-                    currentStroke = listOf(offset)
-                },
-                onDrag = { change, _ ->
-                    currentStroke = currentStroke + change.position
+                change.consume()
+            }, onDragEnd = {
+                if (points.isNotEmpty()) {
+                    val path = Path().apply {
+                        points.forEachIndexed { index, (x, y) ->
+                            if (index == 0) {
+                                moveTo(x, y)
+                            }
 
-                    change.consume()
-                },
-                onDragEnd = {
-                    if (currentStroke.size > 1) {
-                        paths.add(buildPathFromPoints(currentStroke))
+                            lineTo(x, y)
+                        }
                     }
 
-                    currentStroke = emptyList()
+                    paths.add(path)
 
-                    val bitmap = captureCanvasBitmap(canvasSize, paths, strokeColor, strokeWidth)
+                    points.clear()
+                }
 
-                    val skiaBitmap = bitmap.asSkiaBitmap()
-
-                    val bytes = skiaBitmap.readPixels()
-
-                    if (bytes != null) {
-                        val width = skiaBitmap.width
-
-                        val height = skiaBitmap.height
-
-                        val bytesPerPixel = skiaBitmap.bytesPerPixel
-
-                        val bytesPerLine = skiaBitmap.rowBytes
-
-                        coroutineScope.launch {
-                            recognizedText = recognitionService.recognize(
-                                bytes = bytes,
-                                width = width,
-                                height = height,
-                                bytesPerPixel = bytesPerPixel,
-                                bytesPerLine = bytesPerLine
+                onContentChange(canvasBitmap.toPixelMap().buffer)
+            })
+        }) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawIntoCanvas { canvas ->
+                    canvasDrawScope.draw(
+                        density = Density(1f),
+                        layoutDirection = LayoutDirection.Ltr,
+                        canvas = canvas,
+                        size = canvasSize,
+                    ) {
+                        paths.forEach { path ->
+                            drawPath(
+                                path = path,
+                                color = if (mode == DrawingMode.DRAW) Color.Black else Color.White,
+                                style = Stroke(width = strokeWidth, cap = StrokeCap.Round, join = StrokeJoin.Round)
                             )
-                                .fold(onSuccess = { text ->
-                                    text
-                                }, onFailure = { throwable ->
-                                    throwable.message ?: "Unknown error"
-                                })
+                        }
+
+                        if (points.isNotEmpty()) {
+                            drawPath(
+                                path = Path().apply {
+                                    points.forEachIndexed { index, (x, y) ->
+                                        if (index == 0) {
+                                            moveTo(x, y)
+                                        }
+
+                                        lineTo(x, y)
+                                    }
+                                },
+                                color = if (mode == DrawingMode.DRAW) Color.Black else Color.White,
+                                style = Stroke(width = strokeWidth, cap = StrokeCap.Round, join = StrokeJoin.Round)
+                            )
                         }
                     }
                 }
-            )
-        }) {
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                drawRect(color = Color.White, size = size)
-
-                paths.forEach { path ->
-                    drawPath(
-                        path = path,
-                        color = strokeColor,
-                        style = Stroke(width = strokeWidth, cap = StrokeCap.Round, join = StrokeJoin.Round)
-                    )
-                }
-
-                if (currentStroke.size > 1) {
-                    drawPath(
-                        path = buildPathFromPoints(currentStroke),
-                        color = strokeColor,
-                        style = Stroke(width = strokeWidth, cap = StrokeCap.Round, join = StrokeJoin.Round)
-                    )
-                }
             }
         }
-
-        Text(recognizedText, modifier = Modifier.weight(1f))
     }
-}
-
-private fun buildPathFromPoints(points: List<Offset>) = Path().apply {
-    if (points.isNotEmpty()) {
-        moveTo(points.first().x, points.first().y)
-        points.drop(1).forEach { lineTo(it.x, it.y) }
-    }
-}
-
-private fun captureCanvasBitmap(
-    size: Size,
-    paths: List<Path>,
-    strokeColor: Color,
-    strokeWidth: Float
-) = ImageBitmap(size.width.toInt(), size.height.toInt()).let { imageBitmap ->
-    Canvas(imageBitmap).apply {
-        drawRect(0f, 0f, size.width, size.height, Paint().apply { color = Color.White })
-
-        paths.forEach { path ->
-            drawPath(
-                path = path,
-                paint = Paint().apply {
-                    color = strokeColor
-                    this.strokeWidth = strokeWidth
-                    strokeCap = StrokeCap.Round
-                    strokeJoin = StrokeJoin.Round
-                }
-            )
-        }
-    }
-
-    imageBitmap
 }
--- composeApp/src/commonMain/kotlin/io/github/numq/reference/TextReference.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/reference/TextReference.kt
@@ -1,40 +1,69 @@
 package io.github.numq.reference
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun TextReference(text: String) {
+fun TextReference(modifier: Modifier, text: String, onContentChange: (IntArray) -> Unit) {
+    val canvasDrawScope = remember { CanvasDrawScope() }
+
     val textMeasurer = rememberTextMeasurer()
 
-    Canvas(modifier = Modifier.fillMaxSize().alpha(.5f)) {
-        drawRect(color = Color.White, size = size)
+    BoxWithConstraints(modifier = modifier.alpha(0.5f)) {
+        val canvasSize = remember(maxWidth, maxHeight) {
+            Size(maxWidth.value, maxHeight.value)
+        }
 
-        val textLayoutResult = textMeasurer.measure(
-            text = text,
-            style = TextStyle(
-                color = Color.Black,
-                fontSize = minOf(size.width, size.height).sp / 2,
+        val fontSize = remember(canvasSize) {
+            minOf(canvasSize.width, canvasSize.height).sp / 2
+        }
+
+        val textLayoutResult = remember(text, fontSize) {
+            textMeasurer.measure(
+                text = text, style = TextStyle(color = Color.Black, fontSize = fontSize)
             )
-        )
+        }
 
-        val textSize = textLayoutResult.size
+        val canvasBitmap = remember(canvasSize) {
+            ImageBitmap(canvasSize.width.toInt(), canvasSize.height.toInt())
+        }
 
-        drawText(
-            textLayoutResult = textLayoutResult,
-            topLeft = Offset(
-                x = size.width / 2 - textSize.width / 2f,
-                y = size.height / 2 - textSize.height / 2f
-            )
-        )
+        LaunchedEffect(canvasBitmap) {
+            onContentChange(canvasBitmap.toPixelMap().buffer)
+        }
+
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawIntoCanvas { canvas ->
+                canvasDrawScope.draw(
+                    density = Density(1f), layoutDirection = LayoutDirection.Ltr, canvas = canvas, size = canvasSize
+                ) {
+                    drawText(
+                        textLayoutResult = textLayoutResult, topLeft = Offset(
+                            x = size.width / 2 - textLayoutResult.size.width / 2f,
+                            y = size.height / 2 - textLayoutResult.size.height / 2f
+                        )
+                    )
+                }
+            }
+        }
     }
 }
