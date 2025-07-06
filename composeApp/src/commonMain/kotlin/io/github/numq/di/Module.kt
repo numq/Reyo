c7a79e8e6626617b6b34837c0ef4323c9595a170
numq <numq.dev@gmail.com>
2025-07-06 04:24:39 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/di/Module.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/di/Module.kt
@@ -1,19 +1,15 @@
 package io.github.numq.di
 
+import io.github.numq.comparison.ComparisonService
+import io.github.numq.comparison.TextComparisonService
 import io.github.numq.navigation.NavigationFeature
 import io.github.numq.navigation.NavigationReducer
-import io.github.numq.recognition.TextRecognitionService
-import kotlinx.coroutines.runBlocking
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.koin.dsl.onClose
 
 private val recognition = module {
-    single { TextRecognitionService.Implementation() } bind TextRecognitionService::class onClose {
-        runBlocking {
-            it?.close()?.getOrNull()
-        }
-    }
+    single { TextComparisonService() } bind ComparisonService::class
 }
 
 private val navigation = module {
--- composeApp/src/commonMain/kotlin/io/github/numq/navigation/NavigationView.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/navigation/NavigationView.kt
@@ -1,14 +1,37 @@
 package io.github.numq.navigation
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.numq.comparison.ComparisonService
 import io.github.numq.drawing.DrawingCanvas
+import io.github.numq.reference.TextReference
 import org.koin.compose.koinInject
 
 @Composable
-fun NavigationView(feature: NavigationFeature) {
+fun NavigationView(feature: NavigationFeature, comparisonService: ComparisonService = koinInject()) {
+    val coroutineScope = rememberCoroutineScope()
+
+    val letters = remember { "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ" }
+
+    val referenceText = remember { letters.split("").random() }
+
+    val width = 512
+
+    val height = 512
+
+    val (src, setSrc) = remember { mutableStateOf<IntArray?>(null) }
+
+    val (dst, setDst) = remember { mutableStateOf<IntArray?>(null) }
+
+    LaunchedEffect(dst) {
+        if (src != null && dst != null) {
+            comparisonService.compare(src = src, dst = dst, width = width, height = height).getOrThrow()
+        }
+    }
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -16,8 +39,11 @@ fun NavigationView(feature: NavigationFeature) {
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             BoxWithConstraints(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
-                Box(modifier = Modifier.fillMaxSize(.5f).aspectRatio(1f), contentAlignment = Alignment.Center) {
-                    DrawingCanvas(recognitionService = koinInject())
+                Box(
+                    modifier = Modifier.size(width.dp, height.dp).aspectRatio(1f), contentAlignment = Alignment.Center
+                ) {
+                    TextReference(modifier = Modifier.fillMaxSize(), text = referenceText, onContentChange = setSrc)
+                    DrawingCanvas(modifier = Modifier.fillMaxSize(), onContentChange = setDst)
                 }
             }
         }
