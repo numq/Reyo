651961ad63066c3044105cfce5c4f72dd11b163c
numq <numq.dev@gmail.com>
2025-07-12 06:27:42 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/di/Module.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/di/Module.kt
@@ -1,15 +1,20 @@
 package io.github.numq.di
 
 import io.github.numq.comparison.ComparisonService
-import io.github.numq.comparison.TextComparisonService
+import io.github.numq.comparison.tf.TFComparisonService
 import io.github.numq.navigation.NavigationFeature
 import io.github.numq.navigation.NavigationReducer
+import kotlinx.coroutines.runBlocking
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.koin.dsl.onClose
 
 private val recognition = module {
-    single { TextComparisonService() } bind ComparisonService::class
+    single { TFComparisonService() } bind ComparisonService::class onClose {
+        runBlocking {
+            it?.close()?.getOrThrow()
+        }
+    }
 }
 
 private val navigation = module {
