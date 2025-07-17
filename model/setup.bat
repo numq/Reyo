36dd8883a99f2d7a23524fe5014095d6e4572cb0
numq <numq.dev@gmail.com>
2025-07-17 04:30:14 +0300

    Provide refactoring

--- model/setup.bat
+++ model/setup.bat
@@ -13,17 +13,17 @@ if not exist "%CONDA_ROOT%\Scripts\conda.exe" (
 
 echo Checking for %ENV_NAME% environment...
 "%CONDA_ROOT%\Scripts\conda.exe" env list | find "%ENV_NAME%" >nul
+set "ENV_EXISTS=!ERRORLEVEL!"
 
-if %ERRORLEVEL% equ 0 (
+if !ENV_EXISTS! equ 0 (
     echo Updating existing environment...
-    call "%CONDA_ROOT%\Scripts\activate.bat" %ENV_NAME%
-    "%CONDA_ROOT%\Scripts\conda.exe" env update -f "%ENV_FILE%" --prune
+    "%CONDA_ROOT%\Scripts\conda.exe" env update -f "%ENV_FILE%" --prune -n %ENV_NAME%
 ) else (
     echo Creating new environment...
     "%CONDA_ROOT%\Scripts\conda.exe" env create -f "%ENV_FILE%"
 )
 
-if %ERRORLEVEL% neq 0 (
+if !ERRORLEVEL! neq 0 (
     echo [ERROR] Failed to create/update environment
     pause
     exit /b 1
