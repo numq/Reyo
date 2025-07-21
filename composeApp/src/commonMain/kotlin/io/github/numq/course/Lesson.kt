1c262c0322cfec89d5a9ae7b6fcdd749b1d722ba
numq <numq.dev@gmail.com>
2025-07-21 06:01:29 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/course/Lesson.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/course/Lesson.kt
@@ -3,7 +3,5 @@ package io.github.numq.course
 sealed interface Lesson {
     val id: String
 
-    data class Theory(override val id: String, val content: String) : Lesson
-
-    data class Practice(override val id: String, val task: String, val answer: String) : Lesson
+    val steps: List<Step>
 }
---
+++ composeApp/src/commonMain/kotlin/io/github/numq/course/Step.kt
@@ -0,0 +1,17 @@
+package io.github.numq.course
+
+sealed interface Step {
+    val id: String
+
+    val content: String
+
+    data class Theory(override val id: String, override val content: String) : Step
+
+    sealed class Practice private constructor(override val id: String, override val content: String) : Step {
+        data class Listening(override val id: String, override val content: String) : Practice(id, content)
+
+        data class Reading(override val id: String, override val content: String) : Practice(id, content)
+
+        data class Writing(override val id: String, override val content: String) : Practice(id, content)
+    }
+}
